3821f6e25dfebf105b8c1902142273e0f08991c6
Jarosław Pałka <jaroslaw.palka@symentis.pl>
2021-06-25 11:44:29 +0200

    batching map reduce

---
+++ mapreduce-perf/src/main/java/pl/symentis/wordcount/batching/BatchingParallelMapReduceWordCountBenchmark.java
@@ -0,0 +1,60 @@
+package pl.symentis.wordcount.batching;
+
+import org.openjdk.jmh.annotations.*;
+import pl.symentis.mapreduce.batching.BatchingParallelMapReduce;
+import pl.symentis.mapreduce.core.MapReduce;
+import pl.symentis.wordcount.core.Stopwords;
+import pl.symentis.wordcount.core.WordCount;
+
+import java.util.HashMap;
+
+@State(Scope.Benchmark)
+public class BatchingParallelMapReduceWordCountBenchmark {
+
+    @Param({"pl.symentis.wordcount.stopwords.ICUThreadLocalStopwords"})
+    public String stopwordsClass;
+
+    @Param({"8"})
+    public int threadPoolMaxSize;
+
+    @Param({"1000"})
+    public int phaserMaxTasks;
+
+    @Param({"10000"})
+    public int batchSize;
+
+    private WordCount wordCount;
+    private MapReduce mapReduce;
+
+    @SuppressWarnings("unchecked")
+    @Setup(Level.Trial)
+    public void setUp() throws Exception {
+        wordCount = new WordCount
+                .Builder()
+                .withStopwords((Class<? extends Stopwords>) Class.forName(stopwordsClass))
+                .build();
+        mapReduce = new BatchingParallelMapReduce
+                .Builder()
+                .withPhaserMaxTasks(phaserMaxTasks)
+                .withThreadPoolSize(threadPoolMaxSize)
+                .withBatchSize(batchSize)
+                .build();
+    }
+
+    @TearDown(Level.Trial)
+    public void tearDown() {
+        mapReduce.shutdown();
+    }
+
+    @Benchmark
+    public Object countWords() throws Exception {
+        HashMap<String, Long> map = new HashMap<String, Long>();
+        mapReduce.run(
+                wordCount.input(BatchingParallelMapReduceWordCountBenchmark.class.getResourceAsStream("/big.txt")),
+                wordCount.mapper(),
+                wordCount.reducer(),
+                map::put);
+        return map;
+    }
+
+}
