431876c3bfb24e17b2c93640acabb6730fc15425
Jarosław Pałka <jaroslaw.palka@symentis.pl>
2024-09-24 12:34:18 +0200

    bumped versions

--- mapreduce-wordcount/src/test/java/pl/symentis/wordcount/core/SequentialWordCountMapReduceTest.java
+++ mapreduce-wordcount/src/test/java/pl/symentis/wordcount/core/SequentialWordCountMapReduceTest.java
@@ -3,7 +3,7 @@ package pl.symentis.wordcount.core;
 import pl.symentis.mapreduce.core.MapReduce;
 import pl.symentis.mapreduce.core.SequentialMapReduce;
 
-public class SequentialWordCountMapReduceTest implements WordCountMapReduceTest{
+public class SequentialWordCountMapReduceTest implements WordCountMapReduceTest {
     @Override
     public MapReduce mapReduce() {
         return new SequentialMapReduce.Builder().build();
