a7b6e2b1bec401e1eabc8bac175976791deaace1
Jarosław Pałka <jaroslaw.palka@symentis.pl>
2025-06-01 16:31:07 +0200

    upgrading to JDK 24
    (cherry picked from commit 705e9345ce14844abc0ad8f6e419c5b0c8c136db)
    (cherry picked from commit 1212c7efd537e7ef1b5fa7bf40a08a5d95be7aa7)
    (cherry picked from commit f18d979fe73bddb25444c61dd2a810827b81abfd)
    (cherry picked from commit 60720dacfeafc716c2975dbe8fd63f1db6f8236f)

--- mapreduce-wordcount/src/main/java/pl/symentis/wordcount/core/NonCollatorStopwords.java
+++ mapreduce-wordcount/src/main/java/pl/symentis/wordcount/core/NonCollatorStopwords.java
@@ -1,6 +1,7 @@
 package pl.symentis.wordcount.core;
 
 import java.io.BufferedReader;
+import java.io.IOError;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -12,11 +13,13 @@ public class NonCollatorStopwords implements Stopwords {
     private final TreeSet<String> stopwords;
 
     public static Stopwords from(InputStream inputStream) {
+        TreeSet<String> stopwords = new TreeSet<>();
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
             return new NonCollatorStopwords(reader.lines().collect(TreeSet::new, TreeSet::add, TreeSet::addAll));
         } catch (IOException e) {
             throw new UncheckedIOException("cannot read stop words", e);
         }
+        return new NonCollatorStopwords(stopwords);
     }
 
     private NonCollatorStopwords(TreeSet<String> stopwords) {
