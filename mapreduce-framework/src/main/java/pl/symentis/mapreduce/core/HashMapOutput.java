f18d979fe73bddb25444c61dd2a810827b81abfd
Jarosław Pałka <jaroslaw.palka@symentis.pl>
2025-06-01 16:31:07 +0200

    upgrading to JDK 24
    (cherry picked from commit 705e9345ce14844abc0ad8f6e419c5b0c8c136db)
    (cherry picked from commit 1212c7efd537e7ef1b5fa7bf40a08a5d95be7aa7)

--- mapreduce-framework/src/main/java/pl/symentis/mapreduce/core/HashMapOutput.java
+++ mapreduce-framework/src/main/java/pl/symentis/mapreduce/core/HashMapOutput.java
@@ -1,6 +1,11 @@
 package pl.symentis.mapreduce.core;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public final class HashMapOutput<K, V> implements MapperOutput<K, V> {
 
