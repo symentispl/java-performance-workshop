662d79a1c6e177d2dc161972c56a6c35d7211a2b
Jarosław Pałka <jaroslaw.palka@symentis.pl>
2025-08-20 14:13:29 +0200

    added off heap mapreduce + benchmark

---
+++ mapreduce-perf/src/main/java/pl/symentis/wordcount/offheap/OffHeapMapReduceWordCountBenchmark.java
@@ -0,0 +1,65 @@
+package pl.symentis.wordcount.offheap;
+
+import java.util.HashMap;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Level;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.TearDown;
+import pl.symentis.mapreduce.core.MapReduce;
+import pl.symentis.mapreduce.offheap.JavaSerializationStrategy;
+import pl.symentis.mapreduce.offheap.OffHeapBatchingMapReduce;
+import pl.symentis.wordcount.core.Stopwords;
+import pl.symentis.wordcount.core.WordCount;
+
+@State(Scope.Benchmark)
+public class OffHeapMapReduceWordCountBenchmark {
+
+    @Param({"pl.symentis.wordcount.stopwords.ICUThreadLocalStopwords"})
+    public String stopwordsClass;
+
+    @Param({"8"})
+    public int threadPoolMaxSize;
+
+    @Param({"1000"})
+    public int phaserMaxTasks;
+
+    @Param({"10000"})
+    public int batchSize;
+
+    private WordCount wordCount;
+    private MapReduce mapReduce;
+
+    @SuppressWarnings("unchecked")
+    @Setup(Level.Trial)
+    public void setUp() throws Exception {
+        wordCount = new WordCount.Builder()
+                .withStopwords((Class<? extends Stopwords>) Class.forName(stopwordsClass))
+                .build();
+        mapReduce = new OffHeapBatchingMapReduce.Builder()
+                .withPhaserMaxTasks(phaserMaxTasks)
+                .withThreadPoolSize(threadPoolMaxSize)
+                .withBatchSize(batchSize)
+                .withKeySerializer(new JavaSerializationStrategy<>())
+                .withValueSerializer(new JavaSerializationStrategy<>())
+                .build();
+    }
+
+    @TearDown(Level.Trial)
+    public void tearDown() {
+        mapReduce.shutdown();
+    }
+
+    @Benchmark
+    public Object countWords() throws Exception {
+        HashMap<String, Long> result = new HashMap<>();
+        mapReduce.run(
+                wordCount.input(OffHeapMapReduceWordCountBenchmark.class.getResourceAsStream("/big.txt")),
+                wordCount.mapper(),
+                wordCount.reducer(),
+                result::put);
+        return result;
+    }
+}
